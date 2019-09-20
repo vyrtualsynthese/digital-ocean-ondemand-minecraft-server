f503071820cc78f48f0dd8ac7fd964b9ecea2ba1
synthese <vyrtualsynthese@gmail.com>
2019-09-20 15:25:28 +0200

    file provider
    file provider
    
    gitignore

--- mc_serv.tf
+++ mc_serv.tf
@@ -5,12 +5,17 @@ resource "digitalocean_droplet" "mc-server" {
   size = "512mb"
   private_networking = true
   ssh_keys = [
-    "${var.ssh_fingerprint}"
+    var.ssh_fingerprint
   ]
+  provisioner "file" {
+    source      = "minecraft-server.tar.gz"
+    destination = "minecraft-server.tar.gz"
+  }
   connection {
     user = "root"
     type = "ssh"
-    private_key = "${file(var.pvt_key)}"
+    private_key = file(var.pvt_key)
     timeout = "2m"
+    host = digitalocean_droplet.mc-server.ipv4_address
   }
 }
