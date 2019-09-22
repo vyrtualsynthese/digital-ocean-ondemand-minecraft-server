455e9e43d787bb6f480137f5d19a30ca53090189
synthese <vyrtualsynthese@gmail.com>
2019-09-22 22:31:50 +0200

    working to server run

--- mc_serv.tf
+++ mc_serv.tf
@@ -2,7 +2,7 @@ resource "digitalocean_droplet" "mc-server" {
   image = "docker-18-04"
   name = "mc-server"
   region = "fra1"
-  size = "512mb"
+  size = "s-1vcpu-1gb"
   private_networking = true
   ssh_keys = [
     var.ssh_fingerprint
@@ -11,11 +11,22 @@ resource "digitalocean_droplet" "mc-server" {
     source      = "minecraft-server.tar.gz"
     destination = "minecraft-server.tar.gz"
   }
+  provisioner "file" {
+    source      = "docker-compose.yml"
+    destination = "docker-compose.yml"
+  }
   connection {
     user = "root"
     type = "ssh"
     private_key = file(var.pvt_key)
     timeout = "2m"
     host = digitalocean_droplet.mc-server.ipv4_address
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "tar -xvf minecraft-server.tar.gz",
+      "cd minecraft-server",
+      "docker-compose up -d"
+      ]
   }
 }
